dff53758e3dec56174da59623c0d10cdd95197b1
lilianeuwase2 <l.uwase@alustudent.com>
2025-06-27 12:31:55 -0700

    new

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Battery"
-str2 = "Street"
-str1, str2 = "Holberton", "School"
-print("Welcome to {} {}!".format(str1, str2))
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} {str2}!")
