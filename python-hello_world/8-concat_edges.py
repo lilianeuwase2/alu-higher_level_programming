064debea7456de7c49a506ee0e2c2717e40e2352
lilianeuwase2 <l.uwase@alustudent.com>
2025-06-27 03:03:40 -0700

    new

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "object-oriented programming with python"
-
-print(str[0:15] + str[15] + str[16:27] + str[27] + str[28:32] + str[32] + str[33:])
+str1 = "object-oriented programming "
+str2 = "with Python"
+str1 += str2
+print(str1)
