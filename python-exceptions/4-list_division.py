a4d1d460b96652cb89cea064af0d029b4078ff5b
lilianeuwase2 <l.uwase@alustudent.com>
2025-07-05 13:45:46 -0700

    new commit

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            a = my_list_1[i]
+            b = my_list_2[i]
+            division = a / b
+        except ZeroDivisionError:
+            print("division by 0")
+            division = 0
+        except (TypeError, ValueError):
+            print("wrong type")
+            division = 0
+        except IndexError:
+            print("out of range")
+            division = 0
+        finally:
+            result.append(division)
+    return result
