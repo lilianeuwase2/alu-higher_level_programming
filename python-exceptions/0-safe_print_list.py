e548128532113b2cd57b0c4da75a1625989067bf
lilianeuwase2 <l.uwase@alustudent.com>
2025-07-11 04:10:48 -0700

    new

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -9,4 +8,4 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     print()
-    return count   
+    return count
