3483a065ab40534c9a988b5a324e642ab47a021f
lilianeuwase2 <l.uwase@alustudent.com>
2025-07-11 13:45:23 -0700

    new

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,15 +1,54 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
+"""basegeometryclass"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """raises exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value for integer and positive"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+"""class rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """creates rectangle class"""
+    def __init__(self, width, height):
+        """initializes rectangle"""
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height
+
+    def area(self):
+        """returns area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """returns string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+"""class square"""
+
 
 class Square(Rectangle):
-    """Square class with custom string representation."""
-    
+    """creates square"""
+
     def __init__(self, size):
-        """Initializes a Square instance with validated size."""
+        """initializes rectangle"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
-    
+        super().__init__(size, size)
+
     def __str__(self):
-        """Returns a string representation of the square."""
-        return f"[Square] {self.__size}/{self.__size}"
+        """returns string of square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+""" Base Geometry Class inherited"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+""" Program that creates a Rectangle and intance its values """
+
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry."""
-    
+    """ class Rectangle that inherits from BaseGeometry """
     def __init__(self, width, height):
-        """Initializes a Rectangle instance with validated width and height."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """ Constructor """
         self.__width = width
         self.__height = height
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
